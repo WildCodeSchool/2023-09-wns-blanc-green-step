8b0dd8d36107fd9b228e9cb00bb468be89c46ceb
laynaria <pro.laynaria@outlook.fr>
2024-02-01 17:49:33 +0100

    Frontend Initialized with .env.local volume being mounted from frontend/src instead of frontend/ which is strange

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import "@/styles/globals.css";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+  from,
+} from "@apollo/client";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+// function to recreate an uri object for apollo which is needed when using link instead of uri
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_API_LINK,
+});
+
+const client = new ApolloClient({
+  link: from([httpLink]),
+  cache: new InMemoryCache(),
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+// Disabling SSR
+export default dynamic(() => Promise.resolve(App), { ssr: false });
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>Salut les green steppers</h1>
+    </>
+  );
+}
+
+export default Home;
