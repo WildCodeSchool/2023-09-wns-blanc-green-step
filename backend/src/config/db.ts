3f3a2886d042de5e1d01e307176415fdb8f45dcc
laynaria <pro.laynaria@outlook.fr>
2024-02-01 16:11:44 +0100

    dotenv now working for database config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,15 +1,15 @@
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const dataSource = new DataSource({
   type: "postgres",
   host: "db",
   port: 5432,
-  username: "green_step",
-  password: "green_step",
-  database: "green_step_db",
-  //   username: process.env.USERNAME,
-  //   password: process.env.PASSWORD,
-  //   database: process.env.DATABASE,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   entities: ["src/entities/*.ts"],
   synchronize: true,
 });
