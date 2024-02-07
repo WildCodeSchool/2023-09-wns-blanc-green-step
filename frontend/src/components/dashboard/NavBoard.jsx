f767625dede7997d3ddd29f0f5397994ace3c3b2
laynaria <pro.laynaria@outlook.fr>
2024-02-07 12:30:33 +0100

    finished structure of dashboard navbar

--- frontend/src/components/dashboard/NavBoard.jsx
+++ frontend/src/components/dashboard/NavBoard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function NavBoard() {
   return (
-    <nav className="fixed z-10 right-0 top-0 p-5 sm:left-0 bg-green-60 w-fit flex flex-col gap-5 mt-20 sm:mt-0 sm:w-96 text-gray-30 rounded-xl sm:rounded-none sm:h-screen">
+    <nav className="fixed z-10 right-0 top-0 p-5 sm:left-0 bg-green-60 w-fit flex flex-col gap-5 mt-20 sm:mt-0 sm:w-96 text-gray-30 rounded-xl sm:rounded-none h-5/6 sm:h-screen">
       <Link href="/" className="hidden sm:flex gap-8 items-center">
         <img className="w-16" src="/images/logo.png" alt="Green Step Logo" />
 
@@ -18,11 +18,18 @@ function NavBoard() {
         Bienvenue Jean-Paul !
       </p>
 
-      <ul className="font-medium self-center text-sm sm:text-base">
+      <ul className="font-medium self-center text-sm sm:text-base grid gap-5 h-full">
         <Link href="/my-expenses">
           <li className="flex gap-4 items-center">
             <img src="/images/carbon-expenses.png" alt="Carbon Expenses Icon" />
             Mes Dépenses Carbones
+          </li>
+        </Link>
+
+        <Link className="self-end" href="/">
+          <li className="flex gap-4 items-center">
+            <img src="/images/signout.png" alt="Signout Icon" />
+            Déconnexion
           </li>
         </Link>
       </ul>
