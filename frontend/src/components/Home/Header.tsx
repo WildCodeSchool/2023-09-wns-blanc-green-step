b8e61005faf692d8a5789c8d76176102430b638b
matstmr <matstmr@gmail.com>
2024-02-07 11:24:44 +0100

    Add Home page and buttons login / register

---
+++ frontend/src/components/Home/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+    <header className="w-100 h-20 flex justify-center items-center relative">
+        <img src="/images/logo.png" alt="Logo" className="w-16 h-16" />
+    </header>
+    );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,31 @@
+import ButtonLogin from "@/components/Home/ButtonLogin";
+import CardChallenge from "@/components/Home/CardChallenge";
+import Header from "@/components/Home/Header";
+
 function Home() {
   return (
     <>
-      <h1 className="font-poppins font-bold italic text-3xl">
-        Salut les green steppers
-      </h1>
+      <Header />
+      {/* Titre + Background Bleu */}
+      <div className="text-center">
+        <div className="inline-block relative max-w-md mx-auto">
+          <h1 className="font-poppins font-bold italic text-xl relative z-10 pl-1 pr-1">
+            Bienvenue sur Green Step !
+          </h1>
+          <div className="absolute inset-x-0 bottom-[-5px] h-5 bg-secondary-10 z-[-1]"></div>
+        </div>
+
+        <p className="font-poppins text-center text-base mt-5">
+          L’app qui te permet de calculer tes impacts et participer à des éco-challenges
+        </p>
+
+        <div className="flex justify-center mt-5">
+          <img src="/images/photo-accueil.png" alt="Illustration d'accueil" className="w-72" />
+        </div>
+      </div>
+
+      <ButtonLogin />
+      
     </>
   );
 }
