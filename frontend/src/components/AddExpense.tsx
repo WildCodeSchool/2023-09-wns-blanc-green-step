353ac9244901de513329496a8859e6593c245f76
laynaria <pro.laynaria@outlook.fr>
2024-02-09 15:13:55 +0100

    slight debug of css

--- frontend/src/components/AddExpense.tsx
+++ frontend/src/components/AddExpense.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import ModalForm from "./ModalForm";
 
-
 export default function AddExpense() {
   const [isPopUp, setIsPopUp] = useState(false);
 
@@ -11,22 +10,24 @@ export default function AddExpense() {
   const closeModal = () => {
     setIsPopUp(false);
   };
-console.log(isPopUp);
+  console.log(isPopUp);
 
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center fixed bottom-8 right-8 z-10">
         <button
-        type="button"
+          type="button"
           onClick={openModal}
-          className="bg-blue-70 hover:bg-blue-50 font-poppins py-2 px-4 my-2 mx-4 rounded-full">
+          className="bg-blue-70 hover:bg-blue-50 font-poppins py-2 px-4 my-2 mx-4 rounded-full"
+        >
           + CO²
         </button>
         <div className="flex justify-center">
           {isPopUp ? (
-          <ModalForm isOpen={isPopUp} onClose={closeModal}>
-          </ModalForm>
-          ) : ''}
+            <ModalForm isOpen={isPopUp} onClose={closeModal}></ModalForm>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </>
