e3d0c3f7c4cbe92eb0a3989c4c08799806ec5a9a
Mika6413 <michaebony@gmail.com>
2024-03-14 09:42:53 +0100

    test carbonExpense avec fake(mock)

---
+++ backend/src/services/carbonExpense.service.test.ts
@@ -0,0 +1,27 @@
+// import "@testing-library/jest-dom";
+// import * as carbonExpenseService from './carbonExpense.service'; 
+// import { describe, test } from "node:test";
+
+// // Mock dependencies
+// jest('', () => {}).mock('../entities/carbonExpense.entity', () => ({
+//   CarbonExpense: {
+//     findOne: jest.fn(),
+//     find: jest.fn(),
+   
+//   }
+// }));
+
+// describe('CarbonExpense Service', () => {
+//   describe('findCarbonExpenseById', () => {
+//     test('should return null for non-existing ID', async () => {
+//       const result = await carbonExpenseService.findCarbonExpenseById(12345);
+//         expect(1).toBe(2)(1).toBe(2)(result).toBeNull();
+//     });
+//   });
+
+//   describe('findAll', () => {
+//     test('should throw error when no expenses are found', async () => {
+//       await expect(carbonExpenseService.findAll()).rejects.toThrowError('Aucunes dépenses à votre actif');
+//     });
+//   });
+// });
