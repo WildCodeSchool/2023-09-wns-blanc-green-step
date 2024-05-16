5f36bdbeace113fab4ceedb88090ff6f06835c86
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2024-05-16 14:39:57 +0200

    add the modal for one challenge

---
+++ frontend/src/components/ModalForOneEcoChallenge.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from "react";
+import { Button } from "./Button";
+import { Challenge } from "@/types/challengeType.type";
+
+interface ModalChallengeProps {
+  isOpen: boolean;
+  onClose: () => void;
+  challenge: Challenge;
+}
+
+export const ModalForOneEcoChallenge = ({
+  isOpen,
+  onClose,
+  challenge,
+}: ModalChallengeProps) => {
+  useEffect(() => {
+    if (isOpen) {
+      // Ajoute la classe overflow-hidden au body
+      document.body.classList.add("overflow-hidden");
+    }
+
+    // Nettoyage en retirant la classe lorsque le modal se ferme
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]); // Exécute cet effet seulement lorsque la valeur de isOpen change
+
+  if (!isOpen) return null;  // Ne rend rien si le modal n'est pas ouvert
+
+  return (
+    <div className="fixed inset-0 bg-gray-50 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center backdrop-blur-[10px] z-[7]">
+      <div className="relative w-[75%] shadow-lg rounded-md bg-green-100 p-6 sm:w-1/2">
+        {/* Contenu du Modal */}
+        <div className="flex flex-col items-center ">
+          {/* Nom du Challenge */}
+          <h3 className="text-lg font-medium mb-4">{challenge.name}</h3>
+
+          {/* Image */}
+          <img
+            className="w-64 h-64 object-cover mb-4 shadow-xl"
+            src={challenge.image}
+            alt="Challenge"
+          />
+
+          {/* Description et Carbon Saving à côté de l'image */}
+          <div className="flex flex-col justify-between items-center w-full mb-4">
+            <p className="text-sm">{challenge.description}</p>
+            <p className="text-sm mt-5">
+              Carbone sauvé : {challenge.carbon_saving}
+            </p>
+          </div>
+
+          <Button
+            color="bg-blue-40"
+            textsize="text-sm"
+            content="Fermer"
+            onClick={onClose}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/my-ecochallenges.tsx
+++ frontend/src/pages/my-ecochallenges.tsx
@@ -3,6 +3,9 @@ import { gql, useQuery, useLazyQuery } from "@apollo/client";
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import { Expense } from "@/types/expense.type";
+import { Button } from "@/components/Button";
+import { useRouter } from "next/router";
+import { ModalForOneEcoChallenge } from "@/components/ModalForOneEcoChallenge";
 
 const GET_ALL_CHALLENGES = gql`
   query GetChallenges {
@@ -39,10 +42,17 @@ const GET_USER_EXPENSES = gql`
 
 function MyEcochallenges() {
   const { user } = useContext(AuthContext);
+  const router = useRouter();
 
   const [carbonExpenses, setCarbonExpenses] = useState([]);
-  const [expensesByActivity, setExpensesByActivity] = useState({});
+  const [expensesByActivity, setExpensesByActivity] = useState<any>({});
   const [challenges, setChallenges] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedChallenge, setSelectedChallenge] = useState(null);
+
+  const redirectChallenges = () => {
+    router.push("/challenges");
+  };
 
   const { loading, error } = useQuery(GET_USER_EXPENSES, {
     variables: {
@@ -62,10 +72,6 @@ function MyEcochallenges() {
   useEffect(() => {
     getChallenges();
   }, []);
-
-  useEffect(() => {
-    console.log(expensesByActivity);
-  }, [expensesByActivity]);
 
   useEffect(() => {
     const countExpensesByActivity = () => {
@@ -88,6 +94,11 @@ function MyEcochallenges() {
     }
   }, [carbonExpenses]);
 
+  const handleOpenModal = (challenge: any) => {
+    setSelectedChallenge(challenge);
+    setIsModalOpen(true);
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
@@ -96,12 +107,38 @@ function MyEcochallenges() {
       <h1 className="font-bold italic text-xl sm:text-4xl text-center mb-12 relative sm:w-fit after:absolute after:w-full after:inset-x-0 after:bottom-[-8px] after:scale-x-105 sm:after:bottom-[-5px] after:h-5 after:bg-secondary-10 z-[1] after:z-[-1]">
         Mes Ecochallenges
       </h1>
-
-      {challenges.map((challenge: any) => (
-        <p key={challenge.id}>
-          {challenge.name} {challenge.activityType.name}
-        </p>
-      ))}
+      <p className="text-grey-20 ">
+        Tu retrouveras ici tous les challenges que tu peux effectuer pour
+        améliorer ton bilan carbone !
+      </p>
+      <div className="text-grey-20 text-center grid grid-cols-2 gap-4 ">
+        {challenges.map((challenge: any) =>
+          expensesByActivity[challenge.activityType.name] > 1 ? (
+            <div
+              key={challenge.id}
+              className="bg-gray-80 py-5 px-7 m-5 rounded-xl"
+            >
+              <p onClick={() => handleOpenModal(challenge)}>{challenge.name}</p>
+            </div>
+          ) : (
+            ""
+          )
+        )}
+      </div>
+      <Button
+        color="bg-blue-40"
+        textsize="text-sm"
+        style="p-4"
+        content="Accéder à tous les challenges"
+        onClick={redirectChallenges}
+      />
+      {selectedChallenge && (
+        <ModalForOneEcoChallenge
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          challenge={selectedChallenge}
+        />
+      )}
     </section>
   );
 }
