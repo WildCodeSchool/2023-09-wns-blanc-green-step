4c37c108ad0e85078b512844cc016d6f28c2231f
laynaria <pro.laynaria@outlook.fr>
2024-05-16 11:14:24 +0200

    externalyzed friend list search bar in a component

--- frontend/src/pages/friends.tsx
+++ frontend/src/pages/friends.tsx
@@ -3,6 +3,7 @@ import { UserFriend } from "@/types/user.type";
 import { gql, useQuery } from "@apollo/client";
 import { useState, useContext } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
+import FriendSearchBar from "@/components/friendlist/FriendSearchBar";
 
 const GET_ALL_USER_FRIENDS = gql`
   query Query($getFriendsByUserIdId: Float!) {
@@ -91,26 +92,9 @@ export default function FriendsPage() {
           Mes Contacts
         </h1>
 
-        <label
-          className="mb-12 text-gray-10 sm:self-end relative flex
-      items-center justify-center sm:pr-8"
-        >
-          <img
-            className="absolute right-4 sm:right-12"
-            src="/images/magnifer.png"
-            alt="Search icon"
-          />
-
-          <input
-            className="bg-gray-80 py-3 px-8 pl-12 placeholder:text-gray-40 rounded-3xl"
-            type="search"
-            placeholder="Recherche"
-            onChange={(e) =>
-              setFilters({ ...filters, username: e.target.value })
-            }
-          />
-        </label>
+        <FriendSearchBar filters={filters} setFilters={setFilters} />
       </section>
+
       <section className="flex flex-col sm:flex-row sm:flex-wrap items-center sm:items-stretch gap-5 sm:gap-y-12 sm:gap-x-16 w-full sm:w-[90%] sm:m-auto sm:justify-center mb-12">
         {friendsArray
           .filter((friend) => filterOptions(friend))
