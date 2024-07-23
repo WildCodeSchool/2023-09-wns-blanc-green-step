3108881846a82070a1cfd58dfccdc84b99a06bcb
laynaria <pro.laynaria@outlook.fr>
2024-07-23 10:16:36 +0200

    redirection now take into account expiration date of token in isSecured component

--- frontend/src/components/secure/isSecured.tsx
+++ frontend/src/components/secure/isSecured.tsx
@@ -1,14 +1,33 @@
+import { JwtPayload } from "@/types/jwtPayloadType.type";
+import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
 
 export default function isSecured(Component: any) {
   return function IsSecured(props: any) {
+    const { setUser } = useContext(AuthContext);
+
     const router = useRouter();
     const token = localStorage.getItem("token");
+
+    const handleDisconnect = () => {
+      setUser({ id: 0, username: "", email: "", image: "" });
+      localStorage.removeItem("token");
+      router.push("/login");
+    };
 
     useEffect(() => {
       if (!token) {
         router.push("/login");
+      }
+
+      if (token) {
+        const { exp } = jwtDecode(token) as JwtPayload;
+
+        if (exp * 1000 < Date.now()) {
+          handleDisconnect();
+        }
       }
     }, []);
 
